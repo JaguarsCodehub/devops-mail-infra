6d76934fd9d95a7b70984c5e1c076afd7375d34b
JaguarsCodehub <techbrains21@gmail.com>
2025-04-27 12:09:16 +0530

    feat: Added Prometheus and also changed the Metrics

--- src/metrics.ts
+++ src/metrics.ts
@@ -2,13 +2,27 @@ import client from 'prom-client';
 
 export const registerMetrics = new client.Registry();
 
-const memoryUsageGauge = new client.Gauge({
+// Memory usage gauges
+const memoryUsageRSS = new client.Gauge({
   name: 'node_memory_rss_bytes',
-  help: 'Resident Set Size memory in bytes',
+  help: 'Resident Set Size memory',
+});
+const memoryUsageHeapTotal = new client.Gauge({
+  name: 'node_memory_heap_total_bytes',
+  help: 'Heap total',
+});
+const memoryUsageHeapUsed = new client.Gauge({
+  name: 'node_memory_heap_used_bytes',
+  help: 'Heap used',
 });
 
-registerMetrics.registerMetric(memoryUsageGauge);
+registerMetrics.registerMetric(memoryUsageRSS);
+registerMetrics.registerMetric(memoryUsageHeapTotal);
+registerMetrics.registerMetric(memoryUsageHeapUsed);
 
 setInterval(() => {
-  memoryUsageGauge.set(process.memoryUsage().rss);
+  const memUsage = process.memoryUsage();
+  memoryUsageRSS.set(memUsage.rss);
+  memoryUsageHeapTotal.set(memUsage.heapTotal);
+  memoryUsageHeapUsed.set(memUsage.heapUsed);
 }, 5000);
